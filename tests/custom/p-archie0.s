24b7600a330aa873c3d9bfe1e664d7af061bedfb
wentao <g2archie@gmail.com>
2017-03-08 08:20:09 -0800

    working version

---
+++ tests/custom/p-archie0.s
@@ -0,0 +1,84 @@
+fdef archie(inseq:asda){
+  alias seq<char> string;
+  alias seq<top> hahah;
+  alias xiaofeng ku;
+  k:=k+1;
+  j:=0;
+  a:int:=10;
+  c:string:=1;
+  b:char:='a';
+  d:person:='a',"sjakdlaksjdklas",553,334;
+  f:seq<rat>:=[1,2];
+  e:dict<int,char>:={1:'1',2:'2',3:'3'};
+  f:seq<top>:=["dsjadklsa","3iodos"];
+  tdef person{name:string,surname:string,age:int};
+  tdef family{allen:chenxin,chenhao:seq<top>,father:person};
+  return haha*432798+33;
+}:seq<top>;
+
+fdef archie(inseq:asda){
+  alias seq<char> string;
+  alias seq<top> hahah;
+  alias xiaofeng ku;
+  k:=k+1;
+  j:=0;
+  a:int:=10;
+  c:string:=1;
+  b:char:='a';
+  d:person:='a',"sjakdlaksjdklas",553,334;
+  f:seq<rat>:=[1,2];
+  e:dict<int,char>:={1:'1',2:'2',3:'3'};
+  f:seq<top>:=["dsjadklsa","3iodos"];
+  tdef person{name:string,surname:string,age:int};
+  tdef family{allen:chenxin,chenhao:seq<top>,father:person};
+  return haha*432798+33;
+}:seq<top>;
+
+main { 
+  return;
+};
+
+fdef archie(inseq:asda){
+  alias seq<char> string;
+  alias seq<top> hahah;
+  alias xiaofeng ku;
+  k:=k+1;
+  j:=0;
+  a:int:=10;
+  c:string:=1;
+  b:char:='a';
+  d:person:='a',"sjakdlaksjdklas",553,334;
+  f:seq<rat>:=[1,2];
+  e:dict<int,char>:={1:'1',2:'2',3:'3'};
+  f:seq<top>:=["dsjadklsa","3iodos"];
+  tdef person{name:string,surname:string,age:int};
+  tdef family{allen:chenxin,chenhao:seq<top>,father:person};
+  return haha*432798+33;
+}:seq<top>;
+		
+fdef archie(inseq:asda){
+  alias seq<char> string;
+  alias seq<top> hahah;
+  alias xiaofeng ku;
+  k:=(k)+(1);
+  j:=0;
+  wwt:int:=(b)-?T?foo(?T?sum(10,c,d,f,e),bar);
+  a:int:=1*c-dsajkldas+312+(b)-?T?foo(?T?sum(10,c,d,f,e),bar);
+  c:string:=c*c;
+  b:char:='a';
+  d:person:='a',"sjakdlaksjdklas",553,334;
+  f:seq<rat>:=[1,2];
+  e:dict<int,char>:={1:'1',2:'2',3:'3'};
+  f:seq<top>:=["dsjadklsa","3iodos"];
+  tdef person{name:string,surname:string,age:int};
+  tdef family{allen:chenxin,chenhao:seq<top>,father:person};
+  g:int:= ?pred?foo(djsaldklsa,"dsajdljksa",43230423*4328998);
+  d:bool:=TRUE;
+  c:boole:=1=2;
+  zz:top:=s1::s2::[1,3,4]::s45; 
+  #zz3:top:=d::f::e::g::j::[1,3,4];
+  #zz4:seq<int>:=3821931,37298321;
+  djsakl:seq<int>:=d::f::e::g::j::[1,3,4];
+  djslkdjka:seq<int>:=d;
+  return haha*432798+33;
+}:seq<top>;
