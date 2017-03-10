d8f19db62c90ad697e98fde2a05bf492b439a379
wentao <g2archie@gmail.com>
2017-03-10 01:09:32 +0000

    fix#11

--- tests/custom/p-archie0.s
+++ tests/custom/p-archie0.s
@@ -49,13 +49,20 @@ fdef archie(inseq:asda){
   a:int:=10;
   c:string:=1;
   b:char:='a';
-  d:person:='a',"sjakdlaksjdklas",553,334;
+  d:person:='a',"sjakdlaksjdklas",553,334+444/?T?sum(10,c,d,f,e)+?T?sum(10,c,d,f,e)-?T?sum(10,c,d,f,e);
   f:seq<rat>:=[1,2]::[?T?foo(?T?sum(10,c,d,f,e),bar)];
   e:dict<int,char>:={1:'1',2:'2',3:'3'};
   f:seq<top>:=["dsjadklsa","3iodos"];
   tdef person{name:string,surname:string,age:int};
   tdef family{allen:chenxin,chenhao:seq<top>,father:person};
-  return haha*432798+33;
+  return haha*432798+33+?T?sum(10,c,d,f,e)+?T?sum(10,c,d,f,["dsjadklsa","3iodos"]);
+  return T;
+  #return F&&F&&T||F||?T2?sum(10,c,d,f,f);
+  return (?T?sum(10,c,d,f,["dsjadklsa","3iodos"])||(x||x))||(((x||x)&&x)&&?T?sum(10,c,d,f,["dsjadklsa","3iodos"]));
+  return (((x)||x)||x);
+  return ((x=x)||(x=x))&&((x>y)||(z>k));
+  return ?T?sum(10,c,d,f,e)+ferw/2+3;
+
 }:seq<top>;
 		
 fdef archie(inseq:seq<wwt>){
@@ -64,7 +71,7 @@ fdef archie(inseq:seq<wwt>){
   alias xiaofeng ku;
   k:=(k)+(1)*?T?foo(?T?sum(10,c,d,f,e),bar)-dsaklasj/j1leaskldja;
   j:=((((0))));
-  	if (?T?foo(?T?sum(10,c,d,f,e),bar)||?T?foo(?T?sum(10,a[:c],d[b:],f[a:c],e[adsadas+3],g[?T?sum(10,c,d,f,e)*dsaklj+398312-djkasda/3382931]),bar)) then
+  	if (?T?foo(?T?sum(10,c,d,f,e),bar)||?T?foo(?T?sum(10,a[:c],d[b:],f[a:c],e[adsadas+3],g[?T?sum(10,c,d,f,e)*dsaklj+398312-djkasda/3382931]),bar)&&3>4) then
   		return 0;
   	else
   		return 1;
